a022c178015eab3b84a128e1a23bd4e3ff11a414
rafalo1333 <rafal.kaczor.1993@gmail.com>
2015-04-17 11:38:12 +0200

    Added main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+#: -*- encoding: utf-8 -*-
+
+import kivy
+kivy.require('1.9.0')
+
+from kivy.app import App
+
+class ReportCreatorApp(App):
+    pass
+    
+if __name__ == '__main__':
+    ReportCreatorApp().run()
